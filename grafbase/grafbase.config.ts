ccca7eaf98a2c7c111505dabe8f303d1dcf666ea
Antony Jordan Villar Cristobal <antony.villar.rp@viaexperis.pe>
2023-06-29 10:55:29 -0500

    change grafbase model.

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,21 +1,21 @@
 import { g, auth, config } from '@grafbase/sdk';
 
 const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 20 }),
+  name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().optional(),
-  gitHubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
 });
 
 const Project = g.model('Project', {
-  title: g.string().length({ min: 3}),
-  description: g.string(),
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
   image: g.url(),
-  liveSiteUrl: g.url(),
-  gitHubUrl: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
 });
